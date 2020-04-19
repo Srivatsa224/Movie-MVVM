856d10ff5b57c59ef8d558647d4531e60cb44a48
srivatsa224 <srivatsa224@gmail.com>
2020-04-19 23:37:25 +0530

    Added Retrofit library

--- app/src/main/java/com/srivatsa/moviemvvm/data/api/TheMovieDBClient.kt
+++ app/src/main/java/com/srivatsa/moviemvvm/data/api/TheMovieDBClient.kt
@@ -19,35 +19,37 @@ const val POSTER_BASE_URL="https://image.tmdb.org/t/p/w342"
 
 object TheMovieDBClient {
 
-    fun getClient(): Builder {
-        val requestInterceptor=Interceptor{chain ->
+    fun getClient(): TheMovieDBInterface {
 
-            val url=chain.request()
+        val requestInterceptor = Interceptor { chain ->
+            // Interceptor take only one argument which is a lambda function so parenthesis can be omitted
+
+            val url = chain.request()
                 .url()
                 .newBuilder()
                 .addQueryParameter("api_key", API_KEY)
                 .build()
 
-            val request=chain.request()
+            val request = chain.request()
                 .newBuilder()
                 .url(url)
                 .build()
 
-            return@Interceptor chain.proceed(request)
+            return@Interceptor chain.proceed(request)   //explicitly return a value from whit @ annotation. lambda always returns the value of the last expression implicitly
         }
-        val okHttpClient= OkHttpClient.Builder()
+
+        val okHttpClient = OkHttpClient.Builder()
             .addInterceptor(requestInterceptor)
-            .connectTimeout(60,TimeUnit.SECONDS)
+            .connectTimeout(60, TimeUnit.SECONDS)
             .build()
 
-        return Builder()
+        return Retrofit.Builder()
             .client(okHttpClient)
             .baseUrl(BASE_URL)
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
-
-
+            .build()
+            .create(TheMovieDBInterface::class.java)
 
     }
-
 }
